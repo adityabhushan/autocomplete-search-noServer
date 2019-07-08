0c10c8bed57416ec94fbeb48cfce4c5e78695753
Aditya <adityabhushandwivedi@gmail.com>
2019-07-08 16:43:32 +0530

    remove test search router

--- router.js
+++ router.js
@@ -5,11 +5,6 @@
     .config(['$urlRouterProvider', '$stateProvider', function($urlRouterProvider, $stateProvider) {
         $urlRouterProvider.otherwise('/')
         $stateProvider
-            .state('search', {
-                url:'/search',
-                templateUrl: 'templates/search.html',
-                controller: 'searchCtrl'
-            })
             .state('autocomplete', {
                 url: '/',
                 templateUrl: 'templates/autocomplete.html',
